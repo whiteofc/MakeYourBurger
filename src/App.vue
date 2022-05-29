4e484f652b351db44cc08b3588af23e2cb786916
White <whitemusicofc@gmail.com>
2022-05-29 20:11:47 -0300

    Estudos vue JS - Burger

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,47 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <NavBar :logo="logo_src" :alt="app_name"/>
   <router-view/>
+  <Footer />
+
 </template>
 
+<script>
+  import NavBar from "./components/NavBar.vue"
+  import Footer from "./components/Footer.vue"
+export default {
+  components:{
+    NavBar,
+    Footer
+  },
+  data(){
+    return{
+      logo_src: "/imgs/logo.png",
+      app_name: "Make Your Burguer"
+    }
+  }
+}
+</script>
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
+  *{
+    font-family: helvetica;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  .main-container{
+    margin: 50px;
+    min-height: 250px;
+  }
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  h1{
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222;
+  }
+
 </style>
